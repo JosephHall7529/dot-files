68f5cc9e11fbcd5cac633eb65c6673fa3077cd1c
JosephHall <43113966+JosephHall7529@users.noreply.github.com>
2021-02-25 17:46:49 +0000

    Add files via upload

---
+++ aliases.zsh
@@ -0,0 +1,25 @@
+##########################################
+# zsh/oh-my-zsh aliasies
+##########################################
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias reload!="source ~/.zshrc"
+
+
+##########################################
+# Easier Navigation aliases
+##########################################
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+##########################################
+# Program initialization aliases
+##########################################
+
+alias julia="exec '/Applications/Julia-1.5.app/Contents/Resources/julia/bin/julia'"
+alias VLC="open -a VLC"
+alias nb="jupyter notebook"
+alias matlab="exec '/Applications/MATLAB_R2020b.app/bin/matlab'"
+
